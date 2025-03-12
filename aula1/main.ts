c5afb2bc8652ccd561d4fb3f8530c35c2d4c7d1a
giovanne <giovannepm@gmail.com>
2025-03-12 20:07:15 -0300

    fit: criando algoritmo sem modulação

--- aula1/main.ts
+++ aula1/main.ts
@@ -20,19 +20,33 @@ while(option != 9){
 
     switch(option){
         case 1:
-            pokemon.ataque+=2;
+            pokemon.ataque+= Math.floor(Math.random() * 7);
+            pokemon.energia-= Math.floor(Math.random() * 12);
+            if(pokemon.energia <= 0){
+                console.log("energia acabou! fim de jogo.");
+            }else{
+                if(pokemon.ataque >100) pokemon.ataque=100;
+            }
             break;
         case 2:
-            pokemon.defesa+=2;
+            pokemon.defesa+= Math.floor(Math.random() * 7);
+            pokemon.energia-= Math.floor(Math.random() * 12);
+            if(pokemon.energia <= 0){
+                console.log("energia acabou! fim de jogo.");
+            }else{
+                if(pokemon.defesa >100) pokemon.defesa=100;
+            }
             break;
         case 3:
             console.log(pokemon);
             break;
         case 4:
-            pokemon.energia-=20;
+            pokemon.energia-= Math.floor(Math.random() * 10);
+            if(pokemon.energia<=0) console.log("sem energia! fim de jogo.");
             break;
         case 5:
-            pokemon.energia+=5;
+            pokemon.energia+= Math.floor(Math.random() * 4);
+            if(pokemon.energia>100) pokemon.energia=100;
             break;
         case 9:
             console.log("saindo...");
