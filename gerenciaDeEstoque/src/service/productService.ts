e8d6b4500c0af7d638298f4f38247659eec41641
giovanne <giovannepm@gmail.com>
2025-04-08 20:42:24 -0300

    fit: aplicando validações e como o typescript roda

--- gerenciaDeEstoque/src/service/productService.ts
+++ gerenciaDeEstoque/src/service/productService.ts
@@ -1,8 +1,12 @@
-import {Produto} from '../object/Produto'
+const Produto = require('../object/Produto')
+import { Prompt } from 'prompt-sync';
 export function createProduct(nome: string, precoDeCompra: number, custo: number, precoFinal: number, perecivel: boolean): Produto{
 
+    const teclado = prompt
+    var texto;
+    var valor: number;
     while (true) {
-        texto = teclado("Digite o nome: ");
+       texto = teclado("Digite o nome: ");
         
         // Verifica se é string e não contém números
         if (typeof texto === "string" && !/\d/.test(texto)) {
@@ -13,16 +17,47 @@ export function createProduct(nome: string, precoDeCompra: number, custo: number
         console.log("Digite uma string válida (sem números)");
     }
 
-    texto = teclado("digite o preço do produto de fabrica: ")
-    precoDeCompra = texto
-    texto = teclado("digite o custo inicial: ")
-    custo = texto
-    texto = teclado("digite o preço final: ")
-    precoFinal = texto
-    texto = teclado("é perecível? (Y/N): ")
-    perecivel = texto
+    while (true){
+        valor = Number(teclado("digite o preço do produto de fabrica: "))
+
+        if (typeof(precoDeCompra) == "number"){
+            precoDeCompra = valor
+            break
+        }
+
+    }
+
+    while(true){
+        texto = Number(teclado("digite o custo inicial: "))
+
+        if (typeof(texto) == "number"){
+            custo = texto
+            break
+        }
+
+    }
+
+    while(true){
+
+        valor = Number(teclado("digite o preço final: "))
+        
+        if(typeof(precoFinal) == "number"){
+            precoFinal = valor
+            break;
+        }
+    }
+
+    let is_perecivel = teclado("é perecível? (Y/N): ");
+    
+    // Verifica se a resposta existe antes de chamar toUpperCase()
+    if (is_perecivel && is_perecivel.toUpperCase() === "Y") {
+        perecivel = true;
+    } else {
+        perecivel = false; // Definir explicitamente como false caso contrário
+    }
 
     const p: Produto = new Produto(nome,precoDeCompra,custo,precoFinal,perecivel);
     return p
 }
 
+module.exports= {createProduct}
